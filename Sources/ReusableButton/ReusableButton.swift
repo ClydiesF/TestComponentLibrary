76e000db330c49194116b40c4437d116923d897e
Clydies Freeman III <c.edward.freeman@gmail.com>
2022-10-22 12:20:14 -0500

    added color to button

--- Sources/ReusableButton/ReusableButton.swift
+++ Sources/ReusableButton/ReusableButton.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, *)
 public struct ReusableButton: View {
-    public init() {
-        
-    }
+    
     public var body: some View {
-        Text("REUSABLE BUTTON!")
+        Button {
+            print("This is a cool random button")
+        } label: {
+            Text("Random")
+                .padding()
+                .foregroundColor(.white)
+        }
+        .background(Capsule().fill(Color.blue))
+
+        
     }
 }
