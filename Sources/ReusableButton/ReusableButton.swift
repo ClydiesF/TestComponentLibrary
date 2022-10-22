703bd63afc88968b6b5fa38d7804b15f2d7af301
Clydies Freeman III <c.edward.freeman@gmail.com>
2022-10-22 12:22:38 -0500

    added public init

--- Sources/ReusableButton/ReusableButton.swift
+++ Sources/ReusableButton/ReusableButton.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, *)
 public struct ReusableButton: View {
-    
+    public init() {
+        
+    }
     public var body: some View {
         Button {
             print("This is a cool random button")
@@ -19,7 +21,5 @@ public struct ReusableButton: View {
                 .foregroundColor(.white)
         }
         .background(Capsule().fill(Color.blue))
-
-        
     }
 }
